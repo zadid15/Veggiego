b965f398b426267829eaa242572bb080dc1b904a
zadid15 <muhammadzadid061512@gmail.com>
2025-02-04 19:23:52 +0700

    Added API controllers and resources, updated category migration and API routes.

---
+++ app/Http/Resources/Api/ProductResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'description' => $this->description,
+            'slug' => $this->slug,
+            'stock' => $this->stock,
+            'category' => new CategoryResource($this->category),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::apiResource('/products', ProductController::class);
